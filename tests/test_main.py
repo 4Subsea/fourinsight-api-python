e5b280b72aae60e6fbc4fc5eebdb9d1b1257d693
Vegard Rørvik Solum <vrs@4subsea.com>
2020-10-13 15:45:07 +0200

    deteted old test

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from click.testing import CliRunner
-
-from fourinsight.engineroom import __main__ as main
-
-
-def test_init():
-    runner = CliRunner()
-    result = runner.invoke(main.init, "testapp")
-    assert result.output == "Welcome to the machine, testapp\n"
