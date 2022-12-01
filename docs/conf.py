5a963eaf0813d5729a2b443c59210bbca448a1c3
anna-follestad-4ss <aff@4subsea.com>
2022-12-01 13:50:31 +0100

    style: black

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,8 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import inspect
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -14,7 +16,6 @@ import os
 import sys
 from datetime import date
 from importlib import metadata
-import inspect
 
 sys.path.insert(0, os.path.abspath("../"))
 
@@ -42,8 +43,10 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.linkcode"
+    "sphinx.ext.linkcode",
 ]
+
+
 def linkcode_resolve(domain, info):
 
     if domain != "py":
